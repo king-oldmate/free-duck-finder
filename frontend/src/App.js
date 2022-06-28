d6c6898573d65362037fbaf5e7944c8ad27e894d
king-oldmate <amachizzle@gmail.com>
2022-06-28 23:11:47 +1000

    removed functions that were moved to MapView

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MapView from "./components/MapView";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-console.log(process.env.REACT_APP_TOKEN);
-
 const App = () => {
-  const [coordinate, setCoordinate] = useState([]);
-
-  useEffect(() => {
-    let locations;
-    fetch("http://localhost:5000/api/ducks")
-      .then((response) => response.json())
-      .then((data) => (locations = getLocations(data)))
-      .then((locations) => setCoordinate(locations));
-  }, []);
-
-  function getLocations(data) {
-    let locationsList = data.map((item) => {
-      return item.location;
-    });
-    return locationsList;
-  }
-
   return (
     <div>
       <MapView />
-      {coordinate.map((item, index) => {
-        return <p key={index}>{item}</p>;
-      })}
     </div>
   );
 };
