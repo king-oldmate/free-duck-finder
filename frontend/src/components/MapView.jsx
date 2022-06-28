8abd8aed0f910751ddbbdbbeb121a933fe59d0fe
king-oldmate <amachizzle@gmail.com>
2022-06-28 23:10:22 +1000

    map now shows location data from backend

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,8 +1,51 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import Map, { Marker } from "react-map-gl";
 import pin from "./pin.png";
 
 const MapView = () => {
+  const [coordinateList, setCoordinateList] = useState([]);
+
+  useEffect(() => {
+    let ponds;
+    fetch("http://localhost:5000/api/ducks")
+      .then((response) => response.json())
+      .then((data) => (ponds = getLocations(data)))
+      .then((ponds) => setCoordinateList(ponds));
+  }, []);
+
+  function getLocations(data) {
+    let pondList = data.map((pond) => {
+      const pondCoordinates = [pond.longitude, pond.latitude];
+      return pondCoordinates;
+    });
+    return pondList;
+  }
+
+  console.log(coordinateList);
+
+  const pins = useMemo(
+    () =>
+      coordinateList.map((pond, index) => {
+        const [longitude, latitude] = [pond[0], pond[1]];
+        return (
+          <Marker
+            key={`marker-${index}`}
+            longitude={longitude}
+            latitude={latitude}
+            anchor='bottom'
+          >
+            <img
+              src={pin}
+              width='10px'
+              height='15px'
+              alt={`${longitude}, ${latitude}`}
+            />
+          </Marker>
+        );
+      }),
+    [coordinateList]
+  );
+
   return (
     <>
       <Map
@@ -16,9 +59,7 @@ const MapView = () => {
         mapboxAccessToken={process.env.REACT_APP_TOKEN}
       >
         {/* get an array of locations and map through them to place multiple markers */}
-        <Marker longitude={151.1} latitude={-33.6} anchor='bottom'>
-          <img src={pin} width='10px' height='15px' />
-        </Marker>
+        {pins}
       </Map>
     </>
   );
